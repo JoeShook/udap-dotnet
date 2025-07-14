b2b041b4c54fb02520ee262523c3731b8b0484e2
Joe Shook <Joseph.Shook@Surescripts.com>
2025-07-14 15:05:22 -0700

    Allow a Url style Uri to essentually ignore the trailing slash when comparing
    So these two URIs would be considered the same:
    
    https://dev-mtx-interop.meditech.com/
    https://dev-mtx-interop.meditech.com
    
    This shows up when encoding a sub-alt-name is a certificate as
    https://dev-mtx-interop.meditech.com/
    But the iss is set to https://dev-mtx-interop.meditech.com
    
    With this change the two will still validate as the same.
    
    I do need to produce my own certificate material for better long term tests.

--- Udap.Client/Client/UdapClientDiscoveryValidator.cs
+++ Udap.Client/Client/UdapClientDiscoveryValidator.cs
@@ -81,9 +81,11 @@ public class UdapClientDiscoveryValidator : IUdapClientEvents
             .GetSubjectAltNames(n =>
                 n.TagNo == (int)X509Extensions.GeneralNameType.URI) //URI only, by udap.org specification
             .Select(n => new Uri(n.Item2).OriginalString)
-            .ToArray();
+            .ToList();  
 
-        var validatedToken = await ValidateToken(udapServerMetaData, tokenHandler, subjectAltNames, jwt);
+        DenormalizeSubjectAltNames(subjectAltNames);
+
+        var validatedToken = await ValidateToken(udapServerMetaData, tokenHandler, subjectAltNames?.ToArray(), jwt);
 
         if (_publicCertificate == null)
         {
@@ -118,6 +120,36 @@ public class UdapClientDiscoveryValidator : IUdapClientEvents
 
         return true;
 
+    }
+
+    //
+    // Ensure both with and without trailing slash for domain-only URIs
+    // Postel's Law" or the Robustness Principle is being applied here.
+    // "Be conservative in what you send, be liberal in what you accept."
+    // — Jon Postel, RFC 761 (Transmission Control Protocol, 1981)
+    //
+    private static void DenormalizeSubjectAltNames(List<string>? subjectAltNames)
+    {
+        if (subjectAltNames != null)
+        {
+            var additionalNames = new HashSet<string>();
+            foreach (var name in subjectAltNames)
+            {
+                if (Uri.TryCreate(name, UriKind.Absolute, out var uri))
+                {
+                    if (string.IsNullOrEmpty(uri.AbsolutePath) || uri.AbsolutePath == "/")
+                    {
+                        var withSlash = uri.GetLeftPart(UriPartial.Authority) + "/";
+                        var withoutSlash = uri.GetLeftPart(UriPartial.Authority);
+                        if (!subjectAltNames.Contains(withSlash))
+                            additionalNames.Add(withSlash);
+                        if (!subjectAltNames.Contains(withoutSlash))
+                            additionalNames.Add(withoutSlash);
+                    }
+                }
+            }
+            subjectAltNames.AddRange(additionalNames);
+        }
     }
 
     private async Task<TokenValidationResult> ValidateToken(
